0409f3043ee6897a394c6069ea270806cb64f693
aiden7331 <lihtii@hanmail.net>
2019-09-23 20:28:59 +0900

    JPA 중간작업

--- src/main/java/skhu/ht/hotthink/controller/IdeaController.java
+++ src/main/java/skhu/ht/hotthink/controller/IdeaController.java
@@ -3,18 +3,24 @@ package skhu.ht.hotthink.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import skhu.ht.hotthink.domain.Free;
 import skhu.ht.hotthink.domain.Idea;
 import skhu.ht.hotthink.repository.IdeaRepository;
 
 import java.util.List;
 
 @RestController
-public class RealController {
+public class IdeaController {
     @Autowired
     IdeaRepository ideaRepository;
 
     @RequestMapping("realthink")
     public List<Idea> realRead(){
         //return ideaRepository.findBySeqAndTBCategoryCategory(, );
+    }
+
+    @RequestMapping("freethink")
+    public List<Free> freeRead(){
+        return
     }
 }
