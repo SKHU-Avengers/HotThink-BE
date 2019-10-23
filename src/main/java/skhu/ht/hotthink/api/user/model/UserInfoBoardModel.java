199023d4bd4ab92f81bf9b1b8983e0e83e514ab1
YoungGon KIM <33260476+zerogone@users.noreply.github.com>
2019-10-24 04:05:04 +0900

    Merge remote-tracking branch 'origin/zeroGone' into zeroGone
    # Conflicts:
    #	pom.xml
    #	src/main/java/skhu/ht/hotthink/api/domain/Category.java
    #	src/main/java/skhu/ht/hotthink/domain/Category.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Category.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Free.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Idea.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Reply.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Report.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Scrap.java
    #	src/main/java/skhu/ht/hotthink/model/domain/Subscribe.java
    #	src/main/java/skhu/ht/hotthink/repository/UserRepository.java
    #	src/main/resources/application.properties

---
+++ src/main/java/skhu/ht/hotthink/api/user/model/UserInfoBoardModel.java
@@ -0,0 +1,12 @@
+package skhu.ht.hotthink.api.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserInfoBoardModel {
+    protected String title;
+    protected String image;
+    protected String contents;
+}
